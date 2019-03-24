391060d0cd8f91f8114d5480571f7c2c35c791ac
Vineeth Yeluru <vineethyeluru@gmail.com>
2019-03-24 10:59:14 -0500

    rechecking in code

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,11 @@
 import boto3
 
-if __name__ ==  '__main__':
-    session = boto3.Session(profile_name = 'shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name = 'shotty')
+ec2 = session.resource('ec2')
 
+def list_instances():
     for i in ec2.instances.all():
         print(i)
+        
+if __name__ ==  '__main__':
+    list_instances()
